2f9e9ffa5a925f0ef4d2e9344fc996d4300aeef9
William E. Skeith <wes@cs.ccny.cuny.edu>
2024-02-14 19:00:33 -0500

    Add new batch of exercises.

---
+++ practice-problems/04/swap.cpp
@@ -0,0 +1,17 @@
+/* TODO: write a function which takes two integer parameters and swaps
+ * their contents.  That is, if for example x==0,y==1, then after calling
+ * swap(x,y), it should be the case that x==1,y==0.  */
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+/* your answer goes here... */
+
+int main()
+{
+	/* TODO: write a little test here to make sure your function works. */
+	return 0;
+}
+
+// vim:foldlevel=2
