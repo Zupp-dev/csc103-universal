016c155bbec7ef8eda752ce677bdc9a93dde6001
William E. Skeith <wes@cs.ccny.cuny.edu>
2024-03-19 12:24:03 -0400

    Notes from 15th meeting.

---
+++ 15/answers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+
+int main()
+{
+	return 0;
+}
+
+// vim:foldlevel=2
